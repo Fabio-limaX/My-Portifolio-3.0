bd29b49aa329a7c1ab2c0668ef3201ddbd800e07
Fabio Lima <84404364+LimaDev-Max@users.noreply.github.com>
2022-05-29 12:25:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // slide-up script
+    // slide-up inicio
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+        //  scroll  menu items click
         $('html').css("scrollBehavior", "smooth");
     });
     // toggle menu/navbar script
